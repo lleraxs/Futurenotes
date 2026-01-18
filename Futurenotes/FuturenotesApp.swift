7d12ebef1a648992bacc648a02fd0ddae02a03c8
albert <tinaalba@gmail.com>
2026-01-18 10:54:46 +0100

    Initial commit

--- Futurenotes/FuturenotesApp.swift
+++ Futurenotes/FuturenotesApp.swift
@@ -1,17 +1,11 @@
-//
-//  FuturenotesApp.swift
-//  Futurenotes
-//
-//  Created by TA603 on 18.01.26.
-//
-
 import SwiftUI
 
 @main
 struct FuturenotesApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
+    .modelContainer(for: Prediction.self)
