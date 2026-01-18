5a13a487dc1a4abd1d7bef066f1150c18e074d79
albert <tinaalba@gmail.com>
2026-01-18 10:52:21 +0100

    Initial Commit

---
+++ Futurenotes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Futurenotes
+//
+//  Created by TA603 on 18.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Futurenotes/FuturenotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuturenotesApp.swift
+//  Futurenotes
+//
+//  Created by TA603 on 18.01.26.
+//
+
+import SwiftUI
+
+@main
+struct FuturenotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
